9f8928d88b44c8d6768bcd080c83f2a6019130e7
MariaCutipa <129446025+MariaCutipa@users.noreply.github.com>
2024-09-29 21:08:31 -0500

    Add files via upload

---
+++ kafka-redis/app.py
@@ -0,0 +1,24 @@
+from kafka import KafkaConsumer
+from redis import Redis
+import json
+import os
+
+# Configurar Redis
+redis = Redis(host='redis', db=0)
+
+# Configurar Kafka Consumer
+consumer = KafkaConsumer(
+    'votes',
+    bootstrap_servers='54.164.86.184:9092',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+for message in consumer:
+    data = message.value
+    voter_id = data['voter_id']
+    vote = data['vote']
+
+    # Enviar a Redis
+    redis.rpush('votes', json.dumps(data))
+    print(f"Sent vote {vote} from {voter_id} to Redis")
+
